7778c97318678abd8a8fc57e655f2c9f2f0c739a
olivergreenaway <oliver.greenaway@abletech.co.nz>
2019-08-01 15:51:03 +1200

    Add additional config for shortened country codes on the optimized checkout page

--- src/address_form_config/optimized_one_page_checkout.js
+++ src/address_form_config/optimized_one_page_checkout.js
@@ -1,38 +1,78 @@
-export default {
-  label: "Optimized one-page checkout (Early access)",
-  layoutSelectors: ["#micro-app-ng-checkout"],
-  countryIdentifier: '#countryCodeInput',
-  searchIdentifier: "#addressLine1Input",
-  nz: {
-    countryValue: "string:NZ",
-    elements: {
-      address1: '#addressLine1Input',
-      address2: null,
-      suburb: '#addressLine2Input',
-      city: '#cityInput',
-      region: '#provinceInput',
-      postcode: '#postCodeInput'
+export default [
+  {
+    label: "Optimized one-page checkout (Early access)",
+    layoutSelectors: ["#micro-app-ng-checkout"],
+    countryIdentifier: '#countryCodeInput',
+    searchIdentifier: "#addressLine1Input",
+    nz: {
+      countryValue: "string:NZ",
+      elements: {
+        address1: '#addressLine1Input',
+        address2: null,
+        suburb: '#addressLine2Input',
+        city: '#cityInput',
+        region: '#provinceInput',
+        postcode: '#postCodeInput'
+      },
+      regionMappings: null
     },
-    regionMappings: null
+    au: {
+      countryValue: "string:AU",
+      elements: {
+        address1: '#addressLine1Input',
+        address2: '#addressLine2Input',
+        suburb: '#cityInput',
+        state: '#provinceCodeInput',
+        postcode: '#postCodeInput'
+      },
+      stateMappings: {
+        'ACT': 'string:ACT',
+        'NSW': 'string:NSW',
+        'NT' : 'string:NT',
+        'QLD': 'string:QLD',
+        'SA' : 'string:SA',
+        'TAS': 'string:TAS',
+        'VIC': 'string:VIC',
+        'WA' : 'string:WA'
+      }
+    }
   },
-  au: {
-    countryValue: "string:AU",
-    elements: {
-      address1: '#addressLine1Input',
-      address2: '#addressLine2Input',
-      suburb: '#cityInput',
-      state: '#provinceCodeInput',
-      postcode: '#postCodeInput'
+  {
+    label: "Optimized one-page checkout (Early access) (Shortened Country Codes)",
+    layoutSelectors: ["#micro-app-ng-checkout"],
+    countryIdentifier: '#countryCodeInput',
+    searchIdentifier: "#addressLine1Input",
+    nz: {
+      countryValue: "NZ",
+      elements: {
+        address1: '#addressLine1Input',
+        address2: null,
+        suburb: '#addressLine2Input',
+        city: '#cityInput',
+        region: '#provinceInput',
+        postcode: '#postCodeInput'
+      },
+      regionMappings: null
     },
-    stateMappings: {
-      'ACT': 'string:ACT',
-      'NSW': 'string:NSW',
-      'NT' : 'string:NT',
-      'QLD': 'string:QLD',
-      'SA' : 'string:SA',
-      'TAS': 'string:TAS',
-      'VIC': 'string:VIC',
-      'WA' : 'string:WA'
+    au: {
+      countryValue: "AU",
+      elements: {
+        address1: '#addressLine1Input',
+        address2: '#addressLine2Input',
+        suburb: '#cityInput',
+        state: '#provinceCodeInput',
+        postcode: '#postCodeInput'
+      },
+      stateMappings: {
+        'ACT': 'string:ACT',
+        'NSW': 'string:NSW',
+        'NT' : 'string:NT',
+        'QLD': 'string:QLD',
+        'SA' : 'string:SA',
+        'TAS': 'string:TAS',
+        'VIC': 'string:VIC',
+        'WA' : 'string:WA'
+      }
     }
   }
-}
+]
