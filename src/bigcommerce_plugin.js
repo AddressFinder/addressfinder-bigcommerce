d31cec3e2f8d639370f1853f86e47ef03e7a1068
katenorquay <katenorquay@live.com>
2020-10-14 15:32:54 +1300

    ran build and updated version numbers

--- src/bigcommerce_plugin.js
+++ src/bigcommerce_plugin.js
@@ -5,7 +5,7 @@ import ConfigManager from './config_manager'
   class BigcommercePlugin {
     constructor() {
 
-      this.version = "1.5.4"
+      this.version = "1.5.5"
 
       // Manages the mapping of the form configurations to the DOM.
       this.PageManager = null
