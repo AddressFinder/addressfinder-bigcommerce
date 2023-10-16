f03437a9ea744091b343a2a8f3a2e96ab43be1da
bradleyannear <100241767+bradleyannear@users.noreply.github.com>
2023-10-16 14:51:23 +1300

    ADF-2801 Update NZ province ID to match bigcommerce form (#56)

--- src/address_form_config/optimized_one_page_checkout.js
+++ src/address_form_config/optimized_one_page_checkout.js
@@ -10,7 +10,7 @@ export default {
       address2: null,
       suburb: '#addressLine2Input',
       city: '#cityInput',
-      region: '#provinceInput',
+      region: '#provinceCodeInput',
       postcode: '#postCodeInput'
     },
     regionMappings: null
