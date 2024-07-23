9e9c26a05c5733e771962bf74a5ffa91fa05d47b
Erik Fercak <erik.fercak@addressfinder.com>
2024-07-23 14:06:51 +1200

    ADF-3454 Bump version to 2.2.0

--- src/bigcommerce_plugin.js
+++ src/bigcommerce_plugin.js
@@ -5,7 +5,7 @@ import ConfigManager from './config_manager'
   class BigcommercePlugin {
     constructor() {
 
-      this.version = "2.1.0"
+      this.version = "2.2.0"
 
       // Manages the mapping of the form configurations to the DOM.
       this.PageManager = null
